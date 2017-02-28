866881633ebf595cc6cfa86e37d3c05442c14d60
rasmushye <rasmus@hyes.dk>
2017-02-28 16:49:25 +0100

    Importing project

---
+++ HelpfulFriend/Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpfulFriend
+{
+    public class Users
+    {
+        private int UserId;
+        private String Username;
+        private String FirstName;
+        private String LastName;
+        private DateTime BirthDate;
+        private String Email;
+
+        public Users(string username, string fn, string ln, DateTime bd, string mail)
+        {
+            Username = username;
+            FirstName = fn;
+            LastName = ln;
+            BirthDate = bd;
+            Email = mail;
+        }
+    }
+}
